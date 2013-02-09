f7cc12ab651ffd017ec70f9bee6028994eb35bd0
Joey Cabibbo <joseph.cabibbo@gmail.com>
2013-02-09 18:50:35 -0500

    Progress made on determining token kind

--- scripts/lexer.js
+++ scripts/lexer.js
@@ -12,33 +12,39 @@ function Lexer()
 	// Lexically analyze the user submitted source code and produce an array of tokens if successful
 	this.lex = function()
 	{
-	   // Grab the source code text
-	   var sourceCode = $("#sourceCode").val();
+	   // Grab the "trimmmed" source code text
+	   var sourceCode = $("#sourceCode").val().trim();
 
 	   // Split the source code into individual lines
 	   var lineArray = splitSourceByLines(sourceCode);
 
-	   // Iterate through lines
+	   // Iterate lines
 	   for(var i = 0; i < lineArray.length; i++)
 	   {
-	       // Split the current line into tokens
+	       // Split the current line into proper tokens using the ugliest regular expression in the world
     	   var tokenArray = lineArray[i].match(/"[^"]*"|[^\s=\(\)\"\{\};+-]+|[=\(\)\"\{\};+-]/g);
     	   /*
-    	    * "[^"]*"               - match a double quote followed by non-quotes followed by a quote
+    	    * "[^"]*"               - match a double quote followed by non-quotes followed by a double quote
     	    * |                     - OR
     	    * [^\s=\(\)\'\"\{\}\;]+ - match sets of substrings NOT containing these characters
     	    * |                     - OR
     	    * [=\(\)\'\"\{\}\;]     - match these characters
-    	    * g                     - glbal match
+    	    * g                     - global match
     	    */
 
+    	    // Iterate tokens
     	    for(var x = 0; x < tokenArray.length; x++)
     	    {
         	    // Determine kind
         	    // Look for value
         	    // Assign line
-    	    }
 
+        	    //console.log(determineTokenKind(tokenArray[x]));
+
+        	    // Token Construction: kind, name, value, type, line
+        	    //this.tokenList.push()
+        	    _Lexer.tokenList.push(new Token())
+    	    }
 	   }
 
 	   // 2. Get the token name
@@ -49,10 +55,7 @@ function Lexer()
 	   // 7. Add token to token list
 	   // 8. Build symbol table as you go?
 
-	   // Token Construction: kind, name, value, type, line
-/* 	   var a = new Token("TOKEN_ID", "a", 5, "ID", 1); */
-
-	   // Return token list if successful, otherwise return null
+	   // Return token list if successful or just true and false?, otherwise return null
     }
 }
 
@@ -68,6 +71,30 @@ function splitSourceByLines(sourceCode)
       * [\n\r]+|$ - 1 or more instances of newline or carriage return OR end of line
       * g         - global match
       */
+}
+
+// Helper function that takes a token and returns its "kind"
+function determineTokenKind(token)
+{
+    if(isIdentifier(token))
+        return TOKEN_ID;
+    else if(isInteger(token))
+        return TOKEN_INT;
+    else if(isDecimal(token))
+        return TOKEN_DECIMAL;
+}
+
+// Helper function that takes a token string and returns whether it is a valid identifier token
+function isIdentifier(token)
+{
+    return (/^[a-z][a-z0-9]*$/i).test(token);
+    /*
+     *  ^         - start of token
+     *  [a-z]     - any letter a-z
+     *  [a-z0-9]* - 0 or more instances of a-z or 0-9
+     *  $         - end of token
+     *  i         - case insensitive
+     */
 }
 
 // Helper function that takes a token string and returns whether it is a valid integer token
@@ -91,19 +118,6 @@ function isDecimal(token)
      *  |               - OR
      *  [0-9]*(.[0-9]+) - 0 or more instances of any number 0-9 followed by a decimal point and 1 or more intances of any number 0-9
      *  $               - end of token
-     */
-}
-
-// Helper function that takes a token string and returns whether it is a valid identifier token
-function isIdentifier(token)
-{
-    return (/^[a-z][a-z0-9]*$/i).test(token);
-    /*
-     *  ^         - start of token
-     *  [a-z]     - any letter a-z
-     *  [a-z0-9]* - 0 or more instances of a-z or 0-9
-     *  $         - end of token
-     *  i         - case insensitive
      */
 }
 
