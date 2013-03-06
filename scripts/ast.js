4447ce0ac9ce72f8f50f84a5f8eafac9c406cf11
Joey Cabibbo <joseph.cabibbo@gmail.com>
2013-03-06 13:40:20 -0500

    Added ast.js and created global var _AST

---
+++ scripts/ast.js
@@ -0,0 +1,17 @@
+/*  ------------------------------------------------------------
+ *	Filename: ast.js
+ *	Author: Joey Cabibbo
+ *	Requires: globals.js, tree.js
+ *	Description: Abstract Syntax Tree converted from parse's CST
+ *	------------------------------------------------------------ */
+
+// Function that takes a CST from parse and converts it to an AST
+function convertToAST(cst)
+{
+	// The AST we are building
+	var ast = new Tree();
+
+	// DO IT
+
+	return ast;
+}
