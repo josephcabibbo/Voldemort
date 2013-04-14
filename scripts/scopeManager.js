40eb90fd016e5ad1d9b4b13341676138ec7e28aa
Joey Cabibbo <joseph.cabibbo@gmail.com>
2013-04-14 18:15:52 -0400

    Added functionality for the parentScope attribute of the symbol tables

--- scripts/scopeManager.js
+++ scripts/scopeManager.js
@@ -8,7 +8,7 @@
 function ScopeManager()
 {
 	// The current scope environment
-	this.currentScope = 0;
+	this.currentScope = -1;
 	// A list containing all previous scope [least recent - most recent]
 	this.previousScopeList = [];
 
@@ -17,11 +17,13 @@ function ScopeManager()
 	{
 		// Add the current scope to the previous scope list before getting a new scope
 		this.previousScopeList.push(this.currentScope);
+		// Before we get a new scope store it as the previous scope to be used as the parentScope value
+		var previousScope = this.currentScope;
 		// Get a new scope
 		this.currentScope = this.getNextScope();
 
-		// Initialize the scope in the symbol table
-	    _SymbolTableList[this.currentScope] = {};
+		// Initialize the scope in the symbol table with its parentScope value
+	    _SymbolTableList[this.currentScope] = {"parentScope": previousScope};
 	}
 
 	// Function that will leave the current scope and return to the previous scope
@@ -36,7 +38,7 @@ function ScopeManager()
 	{
 		var nextScope = 0;
 
-		// See how many scope we already have, and return the next open index
+		// See how many scopes we already have, and return the next open index
 		for(var i = 0; i < _SymbolTableList.length; i++)
 		{
 			nextScope++;
