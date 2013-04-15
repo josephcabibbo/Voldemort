378c6ba9ca8476c3e968dc9875d80ab73ffb286b
Joey Cabibbo <joseph.cabibbo@gmail.com>
2013-04-15 16:59:20 -0400

    Moved symbol table construction from parse to ast-symbolTable.js

---
+++ scripts/ast-symbolTable.js
@@ -0,0 +1,432 @@
+/*  ----------------------------------------------------------------------------------
+ *	Filename: ast-symbolTable.js
+ *	Author: Joey Cabibbo
+ *	Requires: tree.js, scopeManager.js, errors-warning.js
+ *	Description: Facilitates the creation of an AST and symbol table using lexs stream
+ *				 of tokens looks more or less like a condensed recursive-descent parse
+ *	Note: The AST and symbol table is only created after a successful parse, so we can
+ *		  make assumptionsabout the location of tokens.  This could have been done at
+ *		  the same time as parse but it makes the code ugly and adds complexity.
+ *	---------------------------------------------------------------------------------- */
+
+function createSymbolTableAndAST()
+{
+	// Initialize as a new tree
+	_AST = new Tree();
+	// Set the root
+	_AST.addNode("Program", "branch");
+
+	// The scope manager to be used when creating the symbol table
+	var scopeManager = new ScopeManager();
+
+	// Get the tokens from the Lexer
+	var tokens = _Lexer.tokenList;
+	// Index used for iterating the token list
+	var index = 0;
+
+	// Iterate tokens
+	while(getToken().kind !== TOKEN_EOF)
+	{
+		// We only want additions to the AST when we have an item in First(Statement)
+		switch(getToken().kind)
+		{
+			case TOKEN_PRINT:		 addPrint(); 		 break;
+	        case TOKEN_ID:		 	 addAssignment(); 	 break;
+	        case TOKEN_TYPE:	 	 addVarDecl();		 break;
+	        case TOKEN_OPENBRACKET:  addStatementList(); break;
+	        // This is not part of First(Statement), but we need it in order to end the statementList (scope)
+	        case TOKEN_CLOSEBRACKET: endStatementList(); break;
+
+	        // Should never occur because our parse was successful, but it's here just to avoid an infinite
+	        // loop when we get a heisenbug (which of course will happen), so just move to the next token
+	        default: index++; break;
+		}
+	}
+
+	//
+	// Post creation...
+	//
+
+	// Check for declared but uninitialized variables
+    checkForUninitializedVariables();
+    // Check for variables that have not been unused
+    checkForUnusedVariables();
+    // Display the symbolTable(s)
+	_OutputManager.updateSymbolTable();
+
+	//
+	// AST and Symbol Table creation methods
+	//
+
+	// Function to add a print production to the AST
+	function addPrint()
+	{
+		// print
+		_AST.addNode(getToken().kind, "branch");
+
+		// Look ahead to determine if we need to add an intExpr or just a single digit, string, or id
+		if(getToken(+3).kind === TOKEN_OP)
+		{
+			// Move to the op
+			index += 3;
+			addIntExpr();
+			// Move to the next statement
+			index++;
+		}
+		else
+		{
+			// If the token is an Id we want to add its name to the AST and its symbol table entry in an object
+			// Otherwise (single digit or string) just add the value
+			if(getToken(+2).kind === TOKEN_ID)
+			{
+				// Get the id and current scope
+				var id = getToken(+2).name;
+				var scope = scopeManager.currentScope;
+
+				// Set the identifier as "used" if not done so already
+				setIdentifierAsUsed(id, scope);
+
+				// Add the id and its symbol table entry to the AST in an object
+				_AST.addNode({"id": id, "symbolTableEntry": getSymbolTableEntry(id, scope)}, "leaf");
+			}
+			else
+			{
+				_AST.addNode(getToken(+2).value, "leaf");
+			}
+
+			// Move to the next statement
+			index += 4;
+		}
+
+		// Finished with print
+		_AST.endChildren();
+	}
+
+	// Function to add an assignment production to the AST and its value to the symbol table
+	function addAssignment()
+	{
+		// =
+		_AST.addNode(getToken(+1).value, "branch");
+
+		// Get the id, current scope, and line
+	    var scope = scopeManager.currentScope;
+	    var id = getToken().name;
+	    var line = getToken().line;
+
+		// Add the id and its symbol table entry to the AST in an object
+		_AST.addNode({"id": id, "symbolTableEntry": getSymbolTableEntry(id, scope)}, "leaf");
+
+		// The value to be assigned
+	    var value;
+
+		// Look ahead to determine if we need to add an intExpr or just a single digit, string, or id
+		if(getToken(+3).kind === TOKEN_OP)
+		{
+			// Get the intExpr value for the symbol table
+			value = concatenateIntExpr(index + 2);
+			// Move to the op
+			index += 3;
+			addIntExpr();
+		}
+		else
+		{
+			// If the token is an Id we want to add its name to the AST and its symbol table entry
+			// Otherwise (single digit or string) just add the value
+			if(getToken(+2).kind === TOKEN_ID)
+			{
+				// Add the id and its symbol table entry to the AST in an object
+				_AST.addNode({"id": getToken(+2).name, "symbolTableEntry": getSymbolTableEntry(getToken(+2).name, scope)}, "leaf");
+
+				// Set the identifier value as "used" if not done so already
+				setIdentifierAsUsed(getToken(+2).name, scope);
+
+				// The value is an id
+				value = getToken(+2).name;
+			}
+			else
+			{
+				_AST.addNode(getToken(+2).value, "leaf");
+
+				// The value is a single digit or string
+				value = getToken(+2).value;
+			}
+
+			// Move to the next statement
+			index += 3;
+		}
+
+	    // If the id has been declared in this scope, add the value and mark as "used"
+	    // Otherwise it is an undeclared variable
+	    if(_SymbolTableList[scope] && _SymbolTableList[scope].hasOwnProperty(id))
+	    {
+	    	_SymbolTableList[scope][id].value  = value;
+	    	setIdentifierAsUsed(id, scope);
+	    }
+	    else
+	    {
+		    undeclaredVariableError(id, line); // Found in errors-warnings.js
+	    }
+
+		// Finished with assignment
+		_AST.endChildren();
+	}
+
+	// Function to add a VarDecl production to the AST
+	// Add a symbol table entry for this id
+	function addVarDecl()
+	{
+		// Type (int | string)
+		_AST.addNode(getToken().value, "branch");
+
+		// Get the Id, Type, and the line they are on
+	    var type = getToken().value;
+	    var id   = getToken(+1).name;
+	    var line = getToken(+1).line;
+
+	    // If the current scope is -1 it has not been initialized yet, initialize it.
+	    // This would only be the case when this varDecl is not in a statement list
+	    if(scopeManager.currentScope === -1)
+	    	scopeManager.initializeNewScope();
+
+	    // Get the current scope environment
+	    var scope = scopeManager.currentScope;
+
+	    // If a variable of the same id and scope exists already, it is being redeclared (error)
+	    if(_SymbolTableList[scope].hasOwnProperty(id))
+	    {
+	    	redeclaredVariableError(id, line); // Found in errors-warnings.js
+ 	    }
+	    else
+	    {
+		    // Add all data known at this point to this scope's symbol table entry(format is id : {information})
+		    _SymbolTableList[scope][id] = {"type": type, "line": line, "scope": scope, "isUsed": false};
+		    // Display it in the trace
+		    _OutputManager.addTraceEvent("Added identifier '" + id + "' to the symbol table in scope " + scope);
+	    }
+
+		// Add the id and its symbol table entry to the AST in an object
+		_AST.addNode({"id": id, "symbolTableEntry": getSymbolTableEntry(id, scope)}, "leaf");
+
+		// Move to the next statement
+		index += 2;
+
+		// Finished with VarDecl
+		_AST.endChildren();
+	}
+
+	// Function to add an IntExpr production
+	function addIntExpr()
+	{
+		// op
+		_AST.addNode(getToken().value, "branch");
+		// pre op digit
+		_AST.addNode(getToken(-1).value, "leaf");
+
+		// Look ahead to determine if we need to add another intExpr or just a single digit or id
+		if(getToken(+2).kind === TOKEN_OP)
+		{
+			// Move to the op
+			index += 2;
+			// Handle an additional intExpr
+			addIntExpr();
+		}
+		else
+		{
+			// If the token is an Id we want to add its name to the AST and its symbol table entry
+			// Otherwise (single digit or string) just add the value
+			if(getToken(+1).kind === TOKEN_ID)
+			{
+				// Ge the id and current scope
+				var id = getToken(+1).name;
+				var scope = scopeManager.currentScope;
+
+				// Set the identifier as "used" if not done so already
+				setIdentifierAsUsed(id, scope);
+
+				// Add the id and its symbol table entry to the AST in an object
+				_AST.addNode({"id": id, "symbolTableEntry": getSymbolTableEntry(id, scope)}, "leaf");
+
+			}
+			else
+			{
+				_AST.addNode(getToken(+1).value, "leaf");
+			}
+
+			// Move to the next statement
+			index += 2;
+		}
+
+		// Finished with IntExpr
+		_AST.endChildren();
+	}
+
+	// Function to start a statementList production
+	function addStatementList()
+	{
+		// Add a StatementList branch
+		// Adding this meta-symbol has given me more OCD anxiety than I care to admit...
+		_AST.addNode("StatementList", "branch");
+
+		// Enter a new scope environment
+		scopeManager.initializeNewScope();
+
+		// Move to the next statement
+		index++;
+	}
+
+	// Function to end a statementList production
+	function endStatementList()
+	{
+		// Finished with statementList
+		_AST.endChildren();
+
+		// Leave the current scope environment
+		scopeManager.leaveCurrentScope();
+
+		// Move to the next statement
+		index++;
+	}
+
+	//
+	// Helper functions
+	//
+
+	// Function that gets a token a specified number of indexes away from the current index (does not modify the index)
+	// Positive values for distanceFromCurrentIndex get tokens ahead of the current index in the token list (use +n notation)
+	// Negative values for distanceFromCurrentIndex get tokens behind the current index in the token list (use -n notation)
+	function getToken(distanceFromCurrentIndex)
+	{
+		// I could have just added a negative value but it is more explicit to subtract the absolute value
+
+		// Positive values (look ahead)
+		if(distanceFromCurrentIndex > 0)
+			return tokens[index + distanceFromCurrentIndex];
+		// Negative values (look behind)
+		else if(distanceFromCurrentIndex < 0)
+			return tokens[index - Math.abs(distanceFromCurrentIndex)];
+		// No number or 0 provided, return the current token
+		else
+			return tokens[index];
+	}
+
+	// Function that will return the symbol table entry for the supplied id
+	// If the id is not in the current scope, check the parent scope hierarchy until found
+	function getSymbolTableEntry(id, scope)
+	{
+		// Dafualt entry assuming this entry exists in this scope
+		var entry = _SymbolTableList[scope][id];
+
+		// If the current scope does not have an entry associated with this id, check the parent scope until found
+		// When found, return that symbol table entry
+		if(!_SymbolTableList[scope].hasOwnProperty(id))
+			entry = getSymbolTableEntry(id, _SymbolTableList[scope].parentScope);
+
+		return entry;
+	}
+
+	function setIdentifierAsUsed(id, scope)
+	{
+		// Dafualt entry assuming this entry exists in this scope
+		var entry = _SymbolTableList[scope][id];
+
+		// If the current scope does not have an entry associated with this id call getSymbolTableEntry()
+		// to look in the parent hierarchy until found.  When found set it as "used"
+		if(!_SymbolTableList[scope].hasOwnProperty(id))
+			entry = getSymbolTableEntry(id, scope);
+
+		entry.isUsed = true;
+	}
+
+	// Function that takes the first index of an arbitrarily long IntExpr and concatenates it for value assignment
+	function concatenateIntExpr(index)
+	{
+		// The string int expression return value
+		var exprString = "";
+
+		// Get the first token of the IntExpr
+		var currentToken = tokens[index];
+
+		// The first token has to be an int so append and increment
+		appendToken(currentToken);
+
+		// At this point we can only append the pair of and op and something that follows so keep looping until that format fails
+		while(currentToken.kind === TOKEN_OP)
+		{
+			// Append the op
+			appendToken(currentToken);
+			// Append whatever follows the op
+			appendToken(currentToken);
+		}
+
+		// Internal function to append tokens to the exprString as long as they follow the
+		// format [int] [op] [whatever, but hopefully and int or id or else its a semantic error]
+		function appendToken(token)
+		{
+			// We want the name of an id not its value
+			if(currentToken.kind === TOKEN_ID)
+				exprString += currentToken.name;  // Id
+			else
+				exprString += currentToken.value;  // Integers, ops, and anything else
+
+			// Increment to get the next token
+			currentToken = tokens[++index];
+		}
+
+		 // Return the expression
+		 return exprString;
+	}
+
+	// Function to check the symbol table for declared but uninitialized variables
+	function checkForUninitializedVariables()
+	{
+		// Iterate each scope's symbol table
+		for(var i = 0; i < _SymbolTableList.length; i++)
+		{
+			// Iterate each symbol in the scope's symbol table
+			for(symbol in _SymbolTableList[i])
+			{
+				// If the symbol table entry exists and has no value and is not the parentScope attribute, it is uninitialized.  Warn the user and initialized it to a default value.
+				if(_SymbolTableList[i][symbol].value === undefined && symbol !== "parentScope" )
+				{
+					uninitializedVariableWarning(symbol, i);
+					initializeToDefaultValue(symbol, i)
+				}
+			}
+		}
+	}
+
+	// Function called by checkForUninitializedVariables() to initialize uninitialized variables to default values
+	function initializeToDefaultValue(symbol, scope)
+	{
+		// Get the symbol table entry in the current scope
+		var symbolTableEntry = _SymbolTableList[scope][symbol];
+
+		// Set the value to a default value corresponding to its type
+		switch(symbolTableEntry.type)
+		{
+			case "int":    symbolTableEntry.value = "0"; break;
+			case "string": symbolTableEntry.value = "\"\"";  break;
+		}
+
+		// Display a trace event
+		_OutputManager.addTraceEvent("Initialized identifier '" + symbol + "' to a default " + symbolTableEntry.type + " value")
+	}
+
+	// Function to check the symbol table for unused variables
+	function checkForUnusedVariables()
+	{
+		// Iterate each scope's symbol table
+		for(var i = 0; i < _SymbolTableList.length; i++)
+		{
+			// Iterate each symbol in the scope's symbol table
+			for(symbol in _SymbolTableList[i])
+			{
+				// If the symbol table entry's "isUsed" property is false it is not used.  Warn the user.
+				if(_SymbolTableList[i][symbol].isUsed === false)
+					unusedVariableWarning(symbol, i);
+			}
+		}
+	}
+
+
+}
--- scripts/parser.js
+++ scripts/parser.js
@@ -3,10 +3,7 @@
  *	Author: Joey Cabibbo
  *	Requires: globals.js, outputManager.js, tree.js
  *	Description: A recursive-descent parser that takes the token list to check
- *				 that we have correct statements and creates the symbol table
- *  Notes: It should be known that some assumptions are made about the location
- *		   of tokens when building the symbol table.  If there is a parse error
- *		   the assumptions are probably wrong and the symbol table is discarded
+ *				 that we have correct statement
  *	--------------------------------------------------------------------------- */
 
 function Parser()
@@ -17,8 +14,6 @@ function Parser()
 	this.currentIndex = 0;
 	// Reference to the number of errors found
 	this.errorCount = 0;
-	// The scope manager
-	this.scopeManager = new ScopeManager();
 
 	// General parse call, initializes and starts the recursive descent parse
     this.parse = function()
@@ -27,7 +22,6 @@ function Parser()
     	this.tokens = [];
     	this.currentIndex = 0;
     	this.errorCount = 0;
-    	this.scopeManager = new ScopeManager();
 
     	// Get the stream of tokens from the Lexer
     	this.tokens = _Lexer.tokenList;
@@ -35,24 +29,15 @@ function Parser()
     	// Start the parse
         this.parseProgram();
 
-        // Check for declared but uninitialized variables
-        checkForUninitializedVariables();
-        // Check for variables that have not been unused
-        checkForUnusedVariables();
-
         // Determine if it was a successful parse or a failure
         if(this.errorCount === 0)
         {
 	        _OutputManager.addTraceEvent("Parse successful!", "green");
-	        // Display the symbol tables
-	        _OutputManager.updateSymbolTable();
 	        return true;
         }
         else
         {
 	        _OutputManager.addTraceEvent("Parse failed!", "red");
-	        // Erase the symbol tables because they may be incorrect
-	        _SymbolTableList = [];
 	        return false;
         }
     }
@@ -92,11 +77,9 @@ function Parser()
 	        case TOKEN_TYPE:	 	this.parseVarDecl();
 	        						break;
 
-	        case TOKEN_OPENBRACKET: this.matchToken(TOKEN_OPENBRACKET); // Entering a new scope environment
-	        						this.scopeManager.initializeNewScope();
+	        case TOKEN_OPENBRACKET: this.matchToken(TOKEN_OPENBRACKET);
 	        						this.parseStatementList();
-	        						this.matchToken(TOKEN_CLOSEBRACKET); // Leaving the current scope environment
-	        						this.scopeManager.leaveCurrentScope();
+	        						this.matchToken(TOKEN_CLOSEBRACKET);
 	        						break;
 
 	        // Invalid statement token
@@ -119,28 +102,9 @@ function Parser()
     // Id = Expr
     this.parseAssignment = function()
     {
-    	var id = this.tokens[this.currentIndex].name;
-    	var line = this.tokens[this.currentIndex].line;
-
-	    this.matchToken(TOKEN_ID);
+    	this.matchToken(TOKEN_ID);
 	    this.matchToken(TOKEN_ASSIGN);
-	    // The value of this id will be returned via parseExpr
-	    var expr = this.parseExpr();
-
-	    // Get the current scope
-	    var scope = this.scopeManager.currentScope;
-
-	    // If the id has been declared in this scope, add the value and mark as "used"
-	    // Otherwise it is an undeclared variable
-	    if(_SymbolTableList[scope] && _SymbolTableList[scope].hasOwnProperty(id))
-	    {
-	    	_SymbolTableList[scope][id].value  = expr;
-	    	_SymbolTableList[scope][id].isUsed = true;
-	    }
-	    else
-	    {
-		    undeclaredVariableError(id, line); // Found in errors-warnings.js
-	    }
+	    this.parseExpr();
     }
 
     // Parse a VarDecl production
@@ -149,32 +113,6 @@ function Parser()
     {
     	this.matchToken(TOKEN_TYPE);
 	    this.matchToken(TOKEN_ID);
-
-	    // Get the Id and Type tokens we just matched and get the line they are on
-	    var id   = this.tokens[this.currentIndex - 1].name;
-	    var type = this.tokens[this.currentIndex - 2].value;
-	    var line = this.tokens[this.currentIndex - 1].line;
-
-	    // If the current scope is -1 it has not been initialized yet, initialize it.
-	    // This would only be the case when this varDecl is not in a statement list
-	    if(this.scopeManager.currentScope === -1)
-	    	this.scopeManager.initializeNewScope();
-
-	    // Get the current scope
-	    var scope = this.scopeManager.currentScope;
-
-	    // If a variable of the same id and scope exists already it is being redeclared (error)
-	    if(_SymbolTableList[scope].hasOwnProperty(id))
-	    {
-	    	redeclaredVariableError(id, line); // Found in errors-warnings.js
- 	    }
-	    else
-	    {
-		    // Add all data known at this point to this scope's symbol table entry(format is id : {information})
-		    _SymbolTableList[scope][id] = {"type": type, "line": line, "scope": scope, "isUsed": false};
-		    // Display it in the trace
-		    _OutputManager.addTraceEvent("Added identifier '" + id + "' to the symbol table in scope " + scope);
-	    }
     }
 
     // Parse a StatementList production (where the recursive magic happens)
@@ -210,36 +148,17 @@ function Parser()
     // Id
     this.parseExpr = function()
     {
-    	// The expression to return to varDecl
-    	var expr;
-
     	// Determine which expr we have and parse accordingly
         switch(this.tokens[this.currentIndex].kind)
         {
-	        case TOKEN_INT:		expr = concatenateIntExpr(this.currentIndex); // Must be called before parseIntExpr in order to get the correct index
-	        					this.parseIntExpr();
-	        					break;
-
-	        case TOKEN_STRING:	this.parseStringExpr();
-	        					// Assign the string as the expr value
-	        					expr = this.tokens[this.currentIndex - 1].value
-	        					break;
-
-	        case TOKEN_ID:		this.matchToken(TOKEN_ID);
-	        					// Get the id we just matched
-	        					var id = this.tokens[this.currentIndex - 1].name
-	        					// Assign the id as the expr value
-	        					expr = id;
-	        					// Mark the id as used (if not done so already)
-	        					_SymbolTableList[this.scopeManager.currentScope][id].isUsed = true;
-	        					break;
+	        case TOKEN_INT:		this.parseIntExpr(); 		break;
+	        case TOKEN_STRING:	this.parseStringExpr();		break;
+	        case TOKEN_ID:		this.matchToken(TOKEN_ID); 	break;
 
 	        // Invalid expression
 	        default: invalidExprError(); // Found in errors-warnings.js
 	        		 break;
         }
-
-        return expr;
     }
 
     // Parse an IntExpr production
@@ -299,95 +218,4 @@ function isStatement(tokenKind)
 		   tokenKind === TOKEN_ID	 		||
 		   tokenKind === TOKEN_TYPE  		||
 		   tokenKind === TOKEN_OPENBRACKET;
-}
-
-// Helper function that takes the first index of an arbitrarily long IntExpr and concatenates it for value assignment
-function concatenateIntExpr(index)
-{
-	// The string int expression return value
-	var exprString = "";
-
-	// Get the first token of the IntExpr
-	var currentToken = _Parser.tokens[index];
-
-	// The first token has to be an int so append and increment
-	appendToken(currentToken);
-
-	// At this point we can only append the pair of and op and something that follows so keep looping until that format fails
-	while(currentToken.kind === TOKEN_OP)
-	{
-		// Append the op
-		appendToken(currentToken);
-		// Append whatever follows the op
-		appendToken(currentToken);
-	}
-
-	// Internal function to append tokens to the exprString as long as they follow the
-	// format [int] [op] [whatever, but hopefully and int or id or else its a semantic error]
-	function appendToken(token)
-	{
-		// We want the name of an id not its value
-		if(currentToken.kind === TOKEN_ID)
-			exprString += currentToken.name;  // Id
-		else
-			exprString += currentToken.value;  // Integers, ops, and anything else
-
-		// Increment to get the next token
-		currentToken = _Parser.tokens[++index];
-	}
-
-	 // Return the expression
-	 return exprString;
-}
-
-// Helper function to check the symbol table for declared but uninitialized variables
-function checkForUninitializedVariables()
-{
-	// Iterate each scope's symbol table
-	for(var i = 0; i < _SymbolTableList.length; i++)
-	{
-		// Iterate each symbol in the scope's symbol table
-		for(symbol in _SymbolTableList[i])
-		{
-			// If the symbol table entry exists and has no value and is not the parentScope attribute, it is uninitialized.  Warn the user and initialized it to a default value.
-			if(_SymbolTableList[i][symbol].value === undefined && symbol !== "parentScope" )
-			{
-				uninitializedVariableWarning(symbol, i);
-				initializeToDefaultValue(symbol, i)
-			}
-		}
-	}
-}
-
-// Function called by checkForUninitializedVariables() to initialize uninitialized variables to default values
-function initializeToDefaultValue(symbol, scope)
-{
-	// Get the symbol table entry in the current scope
-	var symbolTableEntry = _SymbolTableList[scope][symbol];
-
-	// Set the value to a default value corresponding to its type
-	switch(symbolTableEntry.type)
-	{
-		case "int":    symbolTableEntry.value = "0"; break;
-		case "string": symbolTableEntry.value = "\"\"";  break;
-	}
-
-	// Display a trace event
-	_OutputManager.addTraceEvent("Initialized identifier '" + symbol + "' to a default " + symbolTableEntry.type + " value")
-}
-
-// Helper function to check the symbol table for unused variables
-function checkForUnusedVariables()
-{
-	// Iterate each scope's symbol table
-	for(var i = 0; i < _SymbolTableList.length; i++)
-	{
-		// Iterate each symbol in the scope's symbol table
-		for(symbol in _SymbolTableList[i])
-		{
-			// If the symbol table entry's "isUsed" property is false it is not used.  Warn the user.
-			if(_SymbolTableList[i][symbol].isUsed === false)
-				unusedVariableWarning(symbol, i);
-		}
-	}
 }
